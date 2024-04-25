805c8677ca9e43617abd08d1d581d68db11a5dac
esdrasgc <esdrasgcdev@gmail.com>
2024-04-24 21:24:55 -0300

    novos campos

--- src/main/java/insper/store/aluno/Aluno.java
+++ src/main/java/insper/store/aluno/Aluno.java
@@ -11,20 +11,25 @@ import java.sql.Date;
 @Getter @Setter @Accessors(fluent = true, chain = true)
 @AllArgsConstructor @NoArgsConstructor
 public class Aluno {
+
     private String id;
     private String nome;
+    private String sobrenome;
     private String email;
     private String genero;
     private Date dataNascimento;
     private String cep;
+    private String cidade;
+    private String uf;
     private String escolaridade;
-    private Integer periodo;
     private String ocupacao;
+    private String estudouEm;
     private Float renda_per_capita;
-    private String modalidade;
-    private Float distancia_em_km;
-    private String oportunidade;
+    private String turno_disponivel;
+    private String disponibilidade_de_deslocamento;
+    private String modalidade_do_ensino;
     private String areas_interesse;
-    private String descricao;
-    
+    private String tipo_oportunidade;
+    private String natureza_instituicao;
+    private String descricao;  
 }
--- src/main/java/insper/store/aluno/AlunoModel.java
+++ src/main/java/insper/store/aluno/AlunoModel.java
@@ -30,6 +30,9 @@ public class AlunoModel {
     @Column(name = "tx_nome")
     private String nome;
 
+    @Column(name = "tx_sobrenome")
+    private String sobrenome;
+
     @Column(name = "tx_email")
     private String email;
 
@@ -42,29 +45,41 @@ public class AlunoModel {
     @Column(name = "tx_cep")
     private String cep;
 
+    @Column(name = "tx_cidade")
+    private String cidade;
+
+    @Column(name = "tx_uf")
+    private String uf;
+
     @Column(name = "tx_escolaridade")
     private String escolaridade;
-
-    @Column(name = "nr_periodo")
-    private Integer periodo;
 
     @Column(name = "tx_ocupacao")
     private String ocupacao;
 
+    @Column(name = "tx_estudou_em")
+    private String estudouEm;
+
     @Column(name = "vl_renda_per_capita")
     private Float renda_per_capita;
 
-    @Column(name = "tx_modalidade")
-    private String modalidade;
+    @Column(name = "tx_turno_disponivel")
+    private String turno_disponivel;
 
-    @Column(name = "vl_distancia_em_km")
-    private Float distancia_em_km;
+    @Column(name = "tx_disponibilidade_deslocamento")
+    private String disponibilidade_de_deslocamento;
 
-    @Column(name = "tx_oportunidade")
-    private String oportunidade;
+    @Column(name = "tx_modalidade_ensino")
+    private String modalidade_do_ensino;
 
     @Column(name = "tx_areas_interesse")
     private String areas_interesse;
+
+    @Column(name = "tx_tipo_oportunidade")
+    private String tipo_oportunidade;
+
+    @Column(name = "tx_natureza_instituicao")
+    private String natureza_instituicao;
 
     @Column(name = "tx_descricao")
     private String descricao;
@@ -72,18 +87,23 @@ public class AlunoModel {
     public AlunoModel(Aluno o) {
         this.id = o.id();
         this.nome = o.nome();
+        this.sobrenome = o.sobrenome();
         this.email = o.email();
         this.genero = o.genero();
         this.dataNascimento = o.dataNascimento();
         this.cep = o.cep();
+        this.cidade = o.cidade();
+        this.uf = o.uf();
         this.escolaridade = o.escolaridade();
-        this.periodo = o.periodo();
         this.ocupacao = o.ocupacao();
+        this.estudouEm = o.estudouEm();
         this.renda_per_capita = o.renda_per_capita();
-        this.modalidade = o.modalidade();
-        this.distancia_em_km = o.distancia_em_km();
-        this.oportunidade = o.oportunidade();
+        this.turno_disponivel = o.turno_disponivel();
+        this.disponibilidade_de_deslocamento = o.disponibilidade_de_deslocamento();
+        this.modalidade_do_ensino = o.modalidade_do_ensino();
         this.areas_interesse = o.areas_interesse();
+        this.tipo_oportunidade = o.tipo_oportunidade();
+        this.natureza_instituicao = o.natureza_instituicao();
         this.descricao = o.descricao();
     }
 
@@ -91,18 +111,23 @@ public class AlunoModel {
         return Aluno.builder()
             .id(id)
             .nome(nome)
+            .sobrenome(sobrenome)
             .email(email)
             .genero(genero)
             .dataNascimento(dataNascimento)
             .cep(cep)
+            .cidade(cidade)
+            .uf(uf)
             .escolaridade(escolaridade)
-            .periodo(periodo)
             .ocupacao(ocupacao)
+            .estudouEm(estudouEm)
             .renda_per_capita(renda_per_capita)
-            .modalidade(modalidade)
-            .distancia_em_km(distancia_em_km)
-            .oportunidade(oportunidade)
+            .turno_disponivel(turno_disponivel)
+            .disponibilidade_de_deslocamento(disponibilidade_de_deslocamento)
+            .modalidade_do_ensino(modalidade_do_ensino)
             .areas_interesse(areas_interesse)
+            .tipo_oportunidade(tipo_oportunidade)
+            .natureza_instituicao(natureza_instituicao)
             .descricao(descricao)
             .build();
     }
