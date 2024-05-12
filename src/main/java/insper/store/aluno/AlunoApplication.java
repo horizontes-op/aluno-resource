86dc0a85c227fe46296418fa7495111a8bb514a5
fernandovs4 <fervsantos3000@gmail.com>
2024-05-11 21:47:26 -0300

    new version

--- src/main/java/insper/store/aluno/AlunoApplication.java
+++ src/main/java/insper/store/aluno/AlunoApplication.java
@@ -2,11 +2,11 @@ package insper.store.aluno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 @SpringBootApplication
 public class AlunoApplication {
 
